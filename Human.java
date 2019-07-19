c5a47221065d3fd4c294a2c3c64b80285ddca3f4
barkemar <martinbarker99@gmail.com>
2019-07-19 16:13:13 -0700

    vid 1 https://www.youtube.com/watch?v=0NPR8GFHNmE

---
+++ Human.java
@@ -0,0 +1,44 @@
+
+/* 
+This is a simple Java program. 
+FileName : "Human.java". 
+Compile with "javac Human.java"
+Run with "java Human"
+*/
+public class Human{
+
+    String name;
+    int age;
+    int heightInInches;
+    String eyeColor;
+
+    //constructor, defines how to create class object
+    //comes by default, we dont need to explicitly define it 
+    public Human(){
+
+    }
+
+    public void speak(){
+        System.out.println("Hello, my name is " + name + ". ");
+        System.out.println("I am " + heightInInches + " tall.");
+        System.out.println("I am " + age + " years old.");
+        System.out.println("My eye color is " + eyeColor + ". ");
+    }
+
+    public void eat(){
+        System.out.println("Eating...");
+    }
+
+    public void walk(){
+        System.out.println("Walking...");
+    }
+
+    public void work(){
+        System.out.println("Working...");
+    }
+    // Your program begins with a call to main(). 
+    public static void main(String args[]) 
+    { 
+        System.out.println("Hello, World"); 
+    } 
+} 
